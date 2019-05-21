dfed7cc3cbddc574bc0fcb58fd161ea5f3e3e7e1
emilydani <38664438+emilydani@users.noreply.github.com>
2019-05-21 08:37:49 -0700

    save

---
+++ MegaDesk-3/SearchAllQuotes.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace MegaDesk_3
+{
+    partial class SearchAllQuotes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MainMenuButton = new System.Windows.Forms.Button();
+            this.SearchComboBox = new System.Windows.Forms.ComboBox();
+            this.Results = new System.Windows.Forms.RichTextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search";
+            // 
+            // MainMenuButton
+            // 
+            this.MainMenuButton.Location = new System.Drawing.Point(39, 321);
+            this.MainMenuButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MainMenuButton.Name = "MainMenuButton";
+            this.MainMenuButton.Size = new System.Drawing.Size(144, 37);
+            this.MainMenuButton.TabIndex = 5;
+            this.MainMenuButton.Text = "Main Menu";
+            this.MainMenuButton.UseVisualStyleBackColor = true;
+            this.MainMenuButton.Click += new System.EventHandler(this.MainMenuButton_Click);
+            // 
+            // SearchComboBox
+            // 
+            this.SearchComboBox.FormattingEnabled = true;
+            this.SearchComboBox.Items.AddRange(new object[] {
+            "Client",
+            "Material"});
+            this.SearchComboBox.Location = new System.Drawing.Point(135, 32);
+            this.SearchComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SearchComboBox.Name = "SearchComboBox";
+            this.SearchComboBox.Size = new System.Drawing.Size(160, 24);
+            this.SearchComboBox.TabIndex = 6;
+            // 
+            // Results
+            // 
+            this.Results.Location = new System.Drawing.Point(39, 86);
+            this.Results.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(636, 214);
+            this.Results.TabIndex = 7;
+            this.Results.Text = "";
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(532, 321);
+            this.Search.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(144, 37);
+            this.Search.TabIndex = 8;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // SearchAllQuotes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 373);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.MainMenuButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchComboBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SearchAllQuotes";
+            this.Text = "SearchAllQuotes";
+            this.Load += new System.EventHandler(this.SearchAllQuotes_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MainMenuButton;
+        private System.Windows.Forms.ComboBox SearchComboBox;
+        private System.Windows.Forms.RichTextBox Results;
+        private System.Windows.Forms.Button Search;
+    }
+}
---
+++ MegaDesk-3/SearchAllQuotes.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_3
+{
+    public partial class SearchAllQuotes : Form
+    {
+        MainMenuForm mainMenu;
+        public string searchBy { get; set; }
+
+        public SearchAllQuotes(MainMenuForm mainMenuForm)
+        {
+            InitializeComponent();
+            mainMenu = mainMenuForm;
+        }
+
+        public string getSearchBy() {
+            return this.searchBy = this.SearchComboBox.Text;
+        }
+
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            DeskQuote deskQuote = new DeskQuote();
+            List<DeskQuote> deskQuotes = new List<DeskQuote>();
+
+            deskQuotes = deskQuote.searchQuotes("Quotes.json", this.searchBy, this);
+
+            this.Results.AppendText("Date" + "\t\t");
+            this.Results.AppendText("Client" + "\t");
+            this.Results.AppendText("Depth" + "\t");
+            this.Results.AppendText("Width" + "\t");
+            this.Results.AppendText("Sice" + "\t");
+            this.Results.AppendText("Material" + "\t\t");
+            this.Results.AppendText("Price" + "\n");
+
+            for (int i = 0; i < deskQuotes.Count; i++) {
+
+                this.Results.AppendText(deskQuotes.ElementAt(i).date + "\t");
+                this.Results.AppendText(deskQuotes.ElementAt(i).clientName + "\t");
+                this.Results.AppendText(deskQuotes.ElementAt(i).desk.width + "\t");
+                this.Results.AppendText(deskQuotes.ElementAt(i).desk.depth + "\t");
+                this.Results.AppendText(deskQuotes.ElementAt(i).desk.size + "\t");
+                if (deskQuotes.ElementAt(i).desk.material.Equals("Rossewood"))
+                {
+                    this.Results.AppendText(deskQuotes.ElementAt(i).desk.material + "\t");
+                }
+                else {
+                    this.Results.AppendText(deskQuotes.ElementAt(i).desk.material + "\t\t");
+                }
+                this.Results.AppendText( "$" + string.Format("{0:n0}",deskQuotes.ElementAt(i).price) + "\n");
+            }
+
+            this.Search.Enabled = false;
+        }
+
+        private void MainMenuButton_Click(object sender, EventArgs e)
+        {
+            mainMenu.Show();
+            this.Hide();
+        }
+
+        private void SearchAllQuotes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
