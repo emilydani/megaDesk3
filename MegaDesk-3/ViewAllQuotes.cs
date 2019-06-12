d8c3dce44765565dcb6727e61b77a79d16551042
emilydani <38664438+emilydani@users.noreply.github.com>
2019-06-12 07:32:01 -0700

    Update ViewAllQuotes.cs

--- MegaDesk-3/ViewAllQuotes.cs
+++ MegaDesk-3/ViewAllQuotes.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,37 +29,40 @@ namespace MegaDesk_3
         }
 
         private void Search_Click(object sender, EventArgs e)
-        { 
-            List<DeskQuote> deskQuotes = new List<DeskQuote>();
+        {
+            quoteGrid.Rows.Clear();
             DeskQuote deskQuote = new DeskQuote();
-            deskQuotes = deskQuote.readJSONFile("Quotes.json");
+            List<DeskQuote> deskQuotes = new List<DeskQuote>();
 
-            this.Results.AppendText("Date" + "\t\t");
-            this.Results.AppendText("Client" + "\t");
-            this.Results.AppendText("Depth" + "\t");
-            this.Results.AppendText("Width" + "\t");
-            this.Results.AppendText("Sice" + "\t");
-            this.Results.AppendText("Material" + "\t\t");
-            this.Results.AppendText("Price" + "\n");
 
-            for (int i = 0; i < deskQuotes.Count; i++) {
+            using (StreamReader r = new StreamReader("Quotes.json"))
+            {
 
-                this.Results.AppendText(deskQuotes.ElementAt(i).date + "\t");
-                this.Results.AppendText(deskQuotes.ElementAt(i).clientName + "\t");
-                this.Results.AppendText(deskQuotes.ElementAt(i).desk.width + "\t");
-                this.Results.AppendText(deskQuotes.ElementAt(i).desk.depth + "\t");
-                this.Results.AppendText(deskQuotes.ElementAt(i).desk.size + "\t");
-                if (deskQuotes.ElementAt(i).desk.material.Equals("Rosewood"))
+
+                while (!r.EndOfStream)
                 {
-                    this.Results.AppendText(deskQuotes.ElementAt(i).desk.material + "\t");
+                    deskQuote = JsonConvert.DeserializeObject<DeskQuote>(r.ReadLine());
+                    deskQuotes.Add(deskQuote);
                 }
-                else {
-                    this.Results.AppendText(deskQuotes.ElementAt(i).desk.material + "\t\t");
-                }
-                this.Results.AppendText( "$" + string.Format("{0:n0}",deskQuotes.ElementAt(i).price) + "\n");
-            }
 
-            this.Search.Enabled = false;
+                foreach (DeskQuote derp in deskQuotes)
+                {
+                        
+                    String[] herp = new String[8];
+                    herp[0] = derp.date;
+                    herp[1] = derp.clientName;
+                    herp[2] = derp.desk.width.ToString();
+                    herp[3] = derp.desk.depth.ToString();
+                    herp[4] = derp.desk.numOfDrawers.ToString();
+                    herp[5] = derp.desk.material;
+                    herp[6] = derp.rushDays.ToString();
+                    herp[7] = derp.price.ToString();
+                    quoteGrid.Rows.Add(herp);
+                    
+
+                }
+
+            }
         }
 
         private void ViewAllQuotes_Load(object sender, EventArgs e)
